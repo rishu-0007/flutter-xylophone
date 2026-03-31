43be07e1e619480cc15bebdb6c10232c943d4df5
Rishav <rishav1072003@gmail.com>
2026-03-31 19:28:25 +0530

    xylophone created with repititive column code

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,94 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Center(
-            child: TextButton(
-                onPressed: (){
-                  final player = AudioPlayer();
-                  player.play(AssetSource('note1.wav'));
-                  print('Pressed');
-                },
-                child: Text('Click me'),
+            child: Column(
+              children:[
+                TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.red),
+                  onPressed: (){
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note1.wav'));
+                    print('Pressed1');
+                  },
+                  child: Text('Click me',
+                  style: TextStyle(
+                    color: Colors.white,
+                   ),
+                  ),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.blue),
+                  onPressed: (){
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note2.wav'));
+                    print('Pressed2');
+                  },
+                  child: Text('Click me',
+                    style: TextStyle(
+                    color: Colors.white,),
+                  ),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.green),
+                  onPressed: (){
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note3.wav'));
+                    print('Pressed3');
+                  },
+                  child: Text('Click me',
+                      style: TextStyle(
+                      color: Colors.white,),
+                  ),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.yellow),
+                  onPressed: (){
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note4.wav'));
+                    print('Pressed4');
+                  },
+                  child: Text('Click me',
+                    style: TextStyle(
+                    color: Colors.white,),
+                   ),
+                 ),
+                TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.orange),
+                  onPressed: (){
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note5.wav'));
+                    print('Pressed5');
+                    },
+                  child: Text('Click me',
+                    style: TextStyle(
+                    color: Colors.white,),
+                     ),
+                  ),
+                TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.pink),
+                  onPressed: (){
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note6.wav'));
+                    print('Pressed6');
+                  },
+                  child: Text('Click me',
+                    style: TextStyle(
+                    color: Colors.white),
+                  ),
+                  ),
+                TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.greenAccent),
+                  onPressed: (){
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note7.wav'));
+                    print('Pressed7');
+                  },
+                  child: Text('Click me',
+                    style: TextStyle(
+                    color: Colors.white,),
+                    ),
+                ),
+              ],
             ),
           )
         ),
@@ -25,3 +106,6 @@ class XylophoneApp extends StatelessWidget {
     );
   }
 }
+
+
+//with a single function playSound();
